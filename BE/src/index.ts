eba521236792e25a59659c90e96e216b001de0d6
Ana Stancic <anet.persa@gmail.com>
2026-03-12 21:03:12 +0100

    Index.ts updated

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,16 +1,29 @@
-import express from 'express' 
-import morgan from 'morgan' 
-import cors from 'cors' 
-import { AppDataSource } from './db' 
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import { AppDataSource } from './db'
+import { CategoryRoute } from './routes/category.route'
+import { FacilityRoute } from './routes/facility.route'
+import { InstrumentRoute } from './routes/instrument.route'
+import { ResearchGroupRoute } from './routes/research-group.route'
+import { ResearcherRoute } from './routes/researcher.route'
+import { ReservationRoute } from './routes/reservation.route'
 
-const app = express() 
-app.use(express.json()) 
-app.use(morgan('short')) 
-app.use(cors) 
+const app = express()
+app.use(express.json())
+app.use(morgan('short'))
+app.use(cors())
 
-AppDataSource.initialize() 
-.then(() => { 
-    const port = process.env.SERVER_PORT || 3000 
-    console.log("Povezan sa bazom") 
-    app.listen(port, () => console.log(`Listening on port ${port}`)) 
-}).catch(e => console.log(e)) 
+app.use('/api/category', CategoryRoute)
+app.use('/api/facility', FacilityRoute)
+app.use('/api/instrument', InstrumentRoute)
+app.use('/api/research-group', ResearchGroupRoute)
+app.use('/api/researcher', ResearcherRoute)
+app.use('/api/reservation', ReservationRoute)
+
+AppDataSource.initialize()
+.then(() => {
+    const port = process.env.SERVER_PORT || 3000
+    console.log("Povezan sa bazom")
+    app.listen(port, () => console.log(`Listening on port ${port}`))
+}).catch(e => console.log(e))
